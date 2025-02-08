5358d666c210c8ee4f7e637bff151d2cfffa843f
Hoerin Doh <hellodhlyn@gmail.com>
2025-02-08 22:08:17 +0900

    remove: 마이그레이션 용 임시 코드 없앰

--- app/routes/api.migrate.tsx
+++ app/routes/api.migrate.tsx
@@ -1,6 +1,5 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { getAuthenticator } from "~/auth/authenticator.server";
-import { migrateFavoriteStudents } from "~/models/content";
 
 export const loader = async ({ request, context }: LoaderFunctionArgs) => {
   const env = context.cloudflare.env;
@@ -8,8 +7,6 @@ export const loader = async ({ request, context }: LoaderFunctionArgs) => {
   if (!me || (me.username !== env.SUPERUSER_NAME)) {
     return new Response("unauthorized", { status: 401 });
   }
-
-  await migrateFavoriteStudents(env);
 
   return new Response("ok", { status: 200 });
 }
