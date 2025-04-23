629679296d79d870d4ce73c4314514b3cdee08dd
Hoerin Doh <hellodhlyn@gmail.com>
2025-04-24 01:34:48 +0900

    feat: 하이랜더 철도학원 추가 🚂

--- app/locales/ko.ts
+++ app/locales/ko.ts
@@ -109,5 +109,6 @@ export const schoolNameLocale: Record<string, string> = {
   redwinter: "붉은겨울 연방학원",
   sakugawa: "사쿠가와 중학교",
   tokiwadai: "토키와다이 중학교",
+  highlander: "하이랜더 철도학원",
   others: "기타 학원",
 };
